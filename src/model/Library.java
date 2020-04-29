4b5e5ec83224734b86bc4edabe80a7277e687d1f
Juan Bustamante <bjuan210302@gmail.com>
2020-04-29 08:34:26 -0500

    added some new attributes and a simple constructor (Library)

--- src/model/Library.java
+++ src/model/Library.java
@@ -1,18 +1,32 @@
 package model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import model.book.*;
 import model.computer.*;
 import model.room.*;
+import model.Person.*;
 
 public class Library {
 
 	private LocalDateTime sysTime;
+	private ArrayList<Person> users;
+	
+	private String nextBookID;
+	private String nextRoomID;
+	private String nextComputerID;
 	
 	private Borrow firstLoan;
 	private Book firstBook;
 	private Computer comTree_root;
 	private Room roomTree_root;
 	
+	public Library() {
+		this.sysTime = LocalDateTime.now();
+		
+		this.nextBookID = "b-1";
+		this.nextRoomID = "r-1";
+		this.nextComputerID = "c-1";
+	}
 }
